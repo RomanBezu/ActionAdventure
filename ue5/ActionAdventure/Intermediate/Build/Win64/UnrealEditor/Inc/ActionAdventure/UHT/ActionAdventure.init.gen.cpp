17a572da835ef41fec123d730ea1fb292f0b9291
MadRom <82387006+RomanBezu@users.noreply.github.com>
2023-10-15 13:48:24 +0300

    InitialSubmit3 Cpp
    InitialSubmit3 Cpp

---
+++ ue5/ActionAdventure/Intermediate/Build/Win64/UnrealEditor/Inc/ActionAdventure/UHT/ActionAdventure.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeActionAdventure_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_ActionAdventure;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_ActionAdventure()
+	{
+		if (!Z_Registration_Info_UPackage__Script_ActionAdventure.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/ActionAdventure",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x2AD39AF4,
+				0x27C0FEC6,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ActionAdventure.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_ActionAdventure.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ActionAdventure(Z_Construct_UPackage__Script_ActionAdventure, TEXT("/Script/ActionAdventure"), Z_Registration_Info_UPackage__Script_ActionAdventure, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x2AD39AF4, 0x27C0FEC6));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
